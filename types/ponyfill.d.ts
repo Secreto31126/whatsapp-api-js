0eb4a57ee8f8fe3bd1cd0b1d48789572fa5a8104
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2022-10-24 21:21:10 -0300

    Added Github Action
    types/ will be ignored in the next commit

--- types/ponyfill.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @package
- * @ignore
- * @returns {(fetch|import("undici/types/fetch").fetch)} The fetch function
- */
-export function pickFetch(): (typeof fetch | typeof import("undici").fetch);
-//# sourceMappingURL=ponyfill.d.ts.map
