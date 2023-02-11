2a7b4abf6416e740a208af62ad1e421a3c51418e
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-02-11 02:43:20 -0300

    eslint approved

--- src/messages/reaction.ts
+++ src/messages/reaction.ts
@@ -1,24 +1,30 @@
 /**
  * Reaction API object
- *
- * @property {string} message_id The message's id to react to
- * @property {string} emoji The reaction emoji
- * @property {"reaction"} [_] The type of the object, for internal use only
  */
 export default class Reaction {
+    /**
+     * The message's id to react to
+     */
     message_id: string;
+    /**
+     * The reaction emoji
+     */
     emoji: string;
+    /**
+     * The type of the object
+     * @internal
+     */
     _?: "reaction";
 
     /**
      * Create a Reaction object for the API
      *
-     * @param {string} message_id The message's id (wamid) to react to
-     * @param {string} emoji The emoji to react with, defaults to empty string to remove a reaction
-     * @throws {Error} If message_id is not provided
-     * @throws {Error} If a non-emoji or more than one emoji is provided
+     * @param message_id - The message's id (wamid) to react to
+     * @param emoji - The emoji to react with, defaults to empty string to remove a reaction
+     * @throws If message_id is not provided
+     * @throws If a non-emoji or more than one emoji is provided
      */
-    constructor(message_id: string, emoji: string = "") {
+    constructor(message_id: string, emoji = "") {
         if (!message_id) throw new Error("Reaction must have a message id");
         if (emoji && !/^\p{Extended_Pictographic}$/u.test(emoji))
             throw new Error("Reaction emoji must be a single emoji");
