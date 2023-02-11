b1698190ae322067d592e800e130ff73d5dd0d33
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-02-11 01:56:44 -0300

    eslint approved
    With a small cheat :)

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,43 +1,98 @@
-const { Contacts } = require("./messages/contacts");
-const { Interactive } = require("./messages/interactive");
-const { Audio, Document, Image, Sticker, Video } = require("./messages/media");
-const Location = require("./messages/location");
-const Reaction = require("./messages/reaction");
-const { Template } = require("./messages/template");
-const Text = require("./messages/text");
+import type { ClientMessage, ClientMessageNames } from "./types";
+import type { Response, RequestInfo } from "undici/types/fetch";
+import type { FormData } from "undici/types/formdata";
 
-const req = require("./ponyfill").pickFetch();
+// TODO: Remove this
+import { fetch } from "undici";
+const req = fetch;
 
 /**
  * Request API object
- *
- * @property {String} messaging_product The messaging product (always "whatsapp")
- * @property {String} type The type of message
- * @property {String} to The user's phone number
- * @property {Object} [context] The message to reply to
- * @property {String} context.message_id The message id to reply to
- * @property {String} [text] The text object stringified
- * @property {String} [audio] The audio object stringified
- * @property {String} [document] The document object stringified
- * @property {String} [image] The image object stringified
- * @property {String} [sticker] The sticker object stringified
- * @property {String} [video] The video object stringified
- * @property {String} [location] The location object stringified
- * @property {String} [contacts] The contacts object stringified
- * @property {String} [interactive] The interactive object stringified
- * @property {String} [template] The template object stringified
- * @property {String} [reaction] The reaction object stringified
+ * @internal
  */
 class Request {
     /**
+     * The messaging product
+     */
+    messaging_product: "whatsapp";
+    /**
+     * The type of message
+     */
+    type: ClientMessageNames;
+    /**
+     * The user's phone number
+     */
+    to: string;
+    /**
+     * The message to reply to
+     */
+    context?: {
+        /**
+         * The message id to reply to
+         */
+        message_id: string;
+    };
+    /**
+     * The text object stringified
+     */
+    text?: string;
+    /**
+     * The audio object stringified
+     */
+    audio?: string;
+    /**
+     * The document object stringified
+     */
+    document?: string;
+    /**
+     * The image object stringified
+     */
+    image?: string;
+    /**
+     * The sticker object stringified
+     */
+    sticker?: string;
+    /**
+     * The video object stringified
+     */
+    video?: string;
+    /**
+     * The location object stringified
+     */
+    location?: string;
+    /**
+     * The contacts object stringified
+     */
+    contacts?: string;
+    /**
+     * The interactive object stringified
+     */
+    interactive?: string;
+    /**
+     * The template object stringified
+     */
+    template?: string;
+    /**
+     * The reaction object stringified
+     */
+    reaction?: string;
+
+    /**
      * Create a Request object for the API
      *
-     * @param {(Text|Audio|Document|Image|Sticker|Video|Location|Contacts|Interactive|Template|Reaction)} object The object to send
-     * @param {String} to The user's phone number
-     * @param {String} [context] The message_id to reply to
+     * @param object - The object to send
+     * @param to - The user's phone number
+     * @param context - The message_id to reply to
      */
-    constructor(object, to, context) {
-        let message = { ...object };
+    constructor(object: ClientMessage, to: string, context?: string) {
+        const message = { ...object };
+
+        if (!message._) {
+            throw new Error(
+                "Unexpected internal error (object._ is not defined)"
+            );
+        }
+
         this.messaging_product = "whatsapp";
         this.type = message._;
         delete message._;
@@ -47,34 +102,46 @@ class Request {
 
         // If the object contains its name as a property, it means it's an array, use it, else use the class
         // This horrible thing comes from Contacts, the only API element which must be an array instead of an object...
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore - I'm not in the mood to fix this
         this[this.type] = JSON.stringify(message[this.type] ?? message);
     }
 }
 
 /**
  * The sendMessage response object
- *
- * @package
- * @ignore
- * @typedef {Object} SendMessageResponse
- * @property {Promise<Response|import("undici/types/fetch").Response>} promise The fetch promise
- * @property {Request} request The request sent to the server
  */
+export type SendMessageResponse = {
+    /**
+     * The fetch promise
+     */
+    promise: Promise<Response>;
+    /**
+     * The request sent to the server
+     */
+    request: Request;
+};
 
 /**
  * Make a message post request to the API
  *
- * @package
- * @ignore
- * @param {String} token The API token
- * @param {String} v The API version
- * @param {String} phoneID The bot's phone id
- * @param {String} to The user's phone number
- * @param {(Text|Audio|Document|Image|Sticker|Video|Location|Contacts|Interactive|Template|Reaction)} object Each type of message requires a specific type of object, for example, the "image" type requires an url and optionally captions. Use the constructors for each specific type of message (contacts, interactive, location, media, template, text)
- * @param {String} context The message id to reply to
- * @returns {SendMessageResponse} An object with the sent request and the fetch promise
+ * @internal
+ * @param token - The API token
+ * @param v - The API version
+ * @param phoneID - The bot's phone id
+ * @param to - The user's phone number
+ * @param object - Each type of message requires a specific type of object, for example, the "image" type requires an url and optionally captions. Use the constructors for each specific type of message (contacts, interactive, location, media, template, text)
+ * @param context - The message id to reply to
+ * @returns An object with the sent request and the fetch promise
  */
-function sendMessage(token, v, phoneID, to, object, context) {
+export function sendMessage(
+    token: string,
+    v: string,
+    phoneID: string,
+    to: string,
+    object: ClientMessage,
+    context: string
+): SendMessageResponse {
     const request = new Request(object, to, context);
 
     // Make the post request
@@ -93,15 +160,19 @@ function sendMessage(token, v, phoneID, to, object, context) {
 /**
  * Mark a message as read
  *
- * @package
- * @ignore
- * @param {String} token The API token
- * @param {String} v The API version
- * @param {String} phoneID The bot's phone id
- * @param {String} message_id The message id
- * @returns {Promise<Response|import("undici/types/fetch").Response>} The fetch promise
+ * @internal
+ * @param token - The API token
+ * @param v - The API version
+ * @param phoneID - The bot's phone id
+ * @param message_id - The message id
+ * @returns The fetch promise
  */
-function readMessage(token, v, phoneID, message_id) {
+export function readMessage(
+    token: string,
+    v: string,
+    phoneID: string,
+    message_id: string
+): Promise<Response> {
     return req(`https://graph.facebook.com/${v}/${phoneID}/messages`, {
         method: "POST",
         headers: {
@@ -119,25 +190,23 @@ function readMessage(token, v, phoneID, message_id) {
 /**
  * Generate a QR code for the bot
  *
- * @package
- * @ignore
- * @param {String} token The API token
- * @param {String} v The API version
- * @param {String} phoneID The bot's phone id
- * @param {String} message The default message in the QR code
- * @param {"png"|"svg"} format The image format of the QR code
- * @returns {Promise<Response|import("undici/types/fetch").Response>} The fetch promise
+ * @internal
+ * @param token - The API token
+ * @param v - The API version
+ * @param phoneID - The bot's phone id
+ * @param message - The default message in the QR code
+ * @param format - The image format of the QR code
+ * @returns The fetch promise
  */
-function makeQR(token, v, phoneID, message, format) {
-    const params = {
-        generate_qr_image: format,
-        prefilled_message: message
-    };
-
+export function makeQR(
+    token: string,
+    v: string,
+    phoneID: string,
+    message: string,
+    format: "png" | "svg"
+): Promise<Response> {
     return req(
-        `https://graph.facebook.com/${v}/${phoneID}/message_qrdls?${new URLSearchParams(
-            params
-        )}`,
+        `https://graph.facebook.com/${v}/${phoneID}/message_qrdls?generate_qr_image=${format}&prefilled_message=${message}`,
         {
             method: "POST",
             headers: {
@@ -150,15 +219,19 @@ function makeQR(token, v, phoneID, message, format) {
 /**
  * Get one or all the QR codes for the bot
  *
- * @package
- * @ignore
- * @param {String} token The API token
- * @param {String} v The API version
- * @param {String} phoneID The bot's phone id
- * @param {String} [id] The QR's id to get. If not specified, all the QR codes will be returned
- * @returns {Promise<Response|import("undici/types/fetch").Response>} The fetch promise
+ * @internal
+ * @param token - The API token
+ * @param v - The API version
+ * @param phoneID - The bot's phone id
+ * @param id - The QR's id to get. If not specified, all the QR codes will be returned
+ * @returns The fetch promise
  */
-function getQR(token, v, phoneID, id) {
+export function getQR(
+    token: string,
+    v: string,
+    phoneID: string,
+    id?: string
+): Promise<Response> {
     return req(
         `https://graph.facebook.com/${v}/${phoneID}/message_qrdls/${id ?? ""}`,
         {
@@ -172,24 +245,23 @@ function getQR(token, v, phoneID, id) {
 /**
  * Update a QR code for the bot
  *
- * @package
- * @ignore
- * @param {String} token The API token
- * @param {String} v The API version
- * @param {String} phoneID The bot's phone id
- * @param {String} id The QR's id to edit
- * @param {String} message The new message for the QR code
- * @returns {Promise<Response|import("undici/types/fetch").Response>} The fetch promise
+ * @internal
+ * @param token - The API token
+ * @param v - The API version
+ * @param phoneID - The bot's phone id
+ * @param id - The QR's id to edit
+ * @param message - The new message for the QR code
+ * @returns The fetch promise
  */
-function updateQR(token, v, phoneID, id, message) {
-    const params = {
-        prefilled_message: message
-    };
-
+export function updateQR(
+    token: string,
+    v: string,
+    phoneID: string,
+    id: string,
+    message: string
+): Promise<Response> {
     return req(
-        `https://graph.facebook.com/${v}/${phoneID}/message_qrdls/${id}?${new URLSearchParams(
-            params
-        )}`,
+        `https://graph.facebook.com/${v}/${phoneID}/message_qrdls/${id}?prefilled_message=${message}`,
         {
             method: "POST",
             headers: {
@@ -202,15 +274,19 @@ function updateQR(token, v, phoneID, id, message) {
 /**
  * Delete a QR code
  *
- * @package
- * @ignore
- * @param {String} token The API token
- * @param {String} v The API version
- * @param {String} phoneID The bot's phone id
- * @param {String} id The QR's id to delete
- * @returns {Promise<Response|import("undici/types/fetch").Response>} The fetch promise
+ * @internal
+ * @param token - The API token
+ * @param v - The API version
+ * @param phoneID - The bot's phone id
+ * @param id - The QR's id to delete
+ * @returns - The fetch promise
  */
-function deleteQR(token, v, phoneID, id) {
+export function deleteQR(
+    token: string,
+    v: string,
+    phoneID: string,
+    id: string
+): Promise<Response> {
     return req(
         `https://graph.facebook.com/${v}/${phoneID}/message_qrdls/${id}`,
         {
@@ -225,18 +301,20 @@ function deleteQR(token, v, phoneID, id) {
 /**
  * Get a Media object
  *
- * @package
- * @ignore
- * @param {String} token The API token
- * @param {String} v The API version
- * @param {String} id The media's id
- * @param {String} [phone_number_id] The business phone number ID
- * @returns {Promise<Response|import("undici/types/fetch").Response>} The fetch promise
+ * @internal
+ * @param token - The API token
+ * @param v - The API version
+ * @param id - The media's id
+ * @param phone_number_id - The business phone number ID
+ * @returns The fetch promise
  */
-function getMedia(token, v, id, phone_number_id) {
-    const params = phone_number_id
-        ? new URLSearchParams({ phone_number_id })
-        : "";
+export function getMedia(
+    token: string,
+    v: string,
+    id: string,
+    phone_number_id?: string
+): Promise<Response> {
+    const params = phone_number_id ? `phone_number_id=${phone_number_id}` : "";
 
     return req(`https://graph.facebook.com/${v}/${id}?${params}`, {
         headers: {
@@ -248,20 +326,23 @@ function getMedia(token, v, id, phone_number_id) {
 /**
  * Upload a Media object
  *
- * @package
- * @ignore
- * @param {String} token The API token
- * @param {String} v The API version
- * @param {String} phoneID The bot's phone id
- * @param {(FormData|import("undici/types/formdata").FormData)} form The media to upload in form format (multipart/form-data)
- * @returns {Promise<Response|import("undici/types/fetch").Response>} The fetch promise
+ * @internal
+ * @param token - The API token
+ * @param v - The API version
+ * @param phoneID - The bot's phone id
+ * @param form - The media to upload in form format (multipart/form-data)
+ * @returns The fetch promise
  */
-function uploadMedia(token, v, phoneID, form) {
+export function uploadMedia(
+    token: string,
+    v: string,
+    phoneID: string,
+    form: FormData
+): Promise<Response> {
     return req(
         `https://graph.facebook.com/${v}/${phoneID}/media?messaging_product=whatsapp`,
         {
             method: "POST",
-            // @ts-ignore
             body: form,
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -274,18 +355,20 @@ function uploadMedia(token, v, phoneID, form) {
 /**
  * Delete a Media
  *
- * @package
- * @ignore
- * @param {String} token The API token
- * @param {String} v The API version
- * @param {String} id The media's id
- * @param {String} [phone_number_id] The business phone number ID
- * @returns {Promise<Response|import("undici/types/fetch").Response>} The fetch promise
+ * @internal
+ * @param token - The API token
+ * @param v - The API version
+ * @param id - The media's id
+ * @param phone_number_id - The business phone number ID
+ * @returns The fetch promise
  */
-function deleteMedia(token, v, id, phone_number_id) {
-    const params = phone_number_id
-        ? new URLSearchParams({ phone_number_id })
-        : "";
+export function deleteMedia(
+    token: string,
+    v: string,
+    id: string,
+    phone_number_id?: string
+): Promise<Response> {
+    const params = phone_number_id ? `phone_number_id=${phone_number_id}` : "";
 
     return req(`https://graph.facebook.com/${v}/${id}?${params}`, {
         method: "DELETE",
@@ -299,30 +382,18 @@ function deleteMedia(token, v, id, phone_number_id) {
  * Make a GET request to any url with the authorization header.
  * Be sure where you are sending the request since it contains the API token.
  *
- * @package
- * @ignore
- * @param {String} token The API token
- * @param {(String|URL)} url The URL to fetch
- * @returns {Promise<Response|import("undici/types/fetch").Response>} The fetch promise
+ * @internal
+ * @param token - The API token
+ * @param url - The URL to fetch
+ * @returns The fetch promise
  */
-function authenticatedRequest(token, url) {
+export function authenticatedRequest(
+    token: string,
+    url: string | RequestInfo
+): Promise<Response> {
     return req(url, {
         headers: {
             Authorization: `Bearer ${token}`
         }
     });
 }
-
-module.exports = {
-    sendMessage,
-    readMessage,
-    makeQR,
-    getQR,
-    updateQR,
-    deleteQR,
-    getMedia,
-    uploadMedia,
-    deleteMedia,
-    authenticatedRequest,
-    Request
-};
