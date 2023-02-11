755a1cb049c918b42e14695c5523e6bb3c25c2ac
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-02-11 02:40:58 -0300

    eslint approved
    The sacred texts:
    @param \{[a-zA-Z_]+\} \[?([a-zA-Z_]+)\]?
    @param $1 -

--- src/messages/location.ts
+++ src/messages/location.ts
@@ -1,32 +1,42 @@
 /**
  * Location API component
- *
- * @property {number} latitude The latitude of the location
- * @property {number} longitude The longitude of the location
- * @property {string} [name] The name of the location
- * @property {string} [address] The address of the location
- * @property {"location"} [_] The type of the object, for internal use only
  */
 export default class Location {
+    /**
+     * The latitude of the location
+     */
     longitude: number;
+    /**
+     * The longitude of the location
+     */
     latitude: number;
+    /**
+     * The name of the location
+     */
     name?: string;
+    /**
+     * The address of the location
+     */
     address?: string;
+    /**
+     * The type of the object
+     * @internal
+     */
     _?: "location";
 
     /**
      * Create a Location object for the API
      *
-     * @param {number} longitude Longitude of the location
-     * @param {number} latitude Latitude of the location
-     * @param {string} [name] Name of the location
-     * @param {string} [address] Address of the location, only displayed if name is present
+     * @param longitude - Longitude of the location
+     * @param latitude - Latitude of the location
+     * @param name - Name of the location
+     * @param address - Address of the location, only displayed if name is present
      */
     constructor(
         longitude: number,
         latitude: number,
-        name: string,
-        address: string
+        name?: string,
+        address?: string
     ) {
         if (!longitude) throw new Error("Longitude must be specified");
         if (!latitude) throw new Error("Latitude must be specified");
