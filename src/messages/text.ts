ea9fcc7ffe4dc24a45e7e79d6e80d01558bbe015
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-05-01 02:36:02 -0300

    This should trigger an alpha release

--- src/messages/text.ts
+++ src/messages/text.ts
@@ -24,7 +24,6 @@ export default class Text implements ClientMessage {
      *
      * @param body - The content of the text message which can contain formatting and URLs which begin with http:// or https://
      * @param preview_url - By default, WhatsApp recognizes URLs and makes them clickable, but you can also include a preview box with more information about the link. Set this field to true if you want to include a URL preview box.
-     * @throws If body is not provided
      * @throws If body is over 4096 characters
      */
     constructor(body: string, preview_url?: boolean) {
