99462b5d6621bdb16fab60d5c66553c9c305db69
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-05-20 19:18:28 -0300

    Idk what I did, idk why I did it...
    But it looks cool :)

--- src/messages/interactive.ts
+++ src/messages/interactive.ts
@@ -338,35 +338,79 @@ export class ActionList
 }
 
 /**
- * Section API object
+ * Section API abstract object
  *
+ * All sections are structured the same way, so this abstract class is used to reduce code duplication
+ *
+ * @remarks
+ * - All sections must have between 1 and N elements
+ * - All sections must have a title if more than 1 section is provided
+ *
+ * @internal
  * @group Interactive
+ *
+ * @typeParam T - The type of the components of the section
+ * @typeParam N - The maximum number of elements in the section
  */
-export class ListSection extends ClientLimitedComponent<Row, 10> {
-    /**
-     * The rows of the section
-     */
-    readonly rows: Row[];
+export abstract class Section<
+    T,
+    N extends number
+> extends ClientLimitedComponent<T, N> {
     /**
      * The title of the section
      */
     readonly title?: string;
 
     /**
-     * Builds a section component for ActionList
+     * Builds a section component
+     *
+     * @param name - The name of the section's type
+     * @param keys_name - The name of the section's keys
+     * @param elements - The elements of the section
+     * @param max - The maximum number of elements in the section
+     * @param title - The title of the section
+     * @param title_length - The maximum length of the title
+     */
+    constructor(
+        name: string,
+        keys_name: string,
+        elements: AtLeastOne<T>,
+        max: N,
+        title?: string,
+        title_length = 24
+    ) {
+        super(name, keys_name, elements, max);
+
+        if (title && title.length > title_length)
+            throw new Error(
+                `${name} title must be ${title_length} characters or less`
+            );
+
+        if (title) this.title = title;
+    }
+}
+
+/**
+ * Section API object
+ *
+ * @group Interactive
+ */
+export class ListSection extends Section<Row, 10> {
+    /**
+     * The rows of the section
+     */
+    readonly rows: Row[];
+
+    /**
+     * Builds a list section component for ActionList
      *
      * @param title - Title of the section, only required if there are more than one section
-     * @param rows - Rows of the section
+     * @param rows - Rows of the list section
      * @throws If title is over 24 characters if provided
      * @throws If more than 10 rows are provided
      */
-    constructor(title: string, ...rows: AtLeastOne<Row>) {
-        super("Section", "rows", rows, 10);
-
-        if (title && title.length > 24)
-            throw new Error("Section title must be 24 characters or less");
-
-        if (title) this.title = title;
+    constructor(title: string | undefined, ...rows: AtLeastOne<Row>) {
+        super("ListSection", "rows", rows, 10, title);
         this.rows = rows;
     }
 }
@@ -391,7 +435,7 @@ export class Row {
     readonly description?: string;
 
     /**
-     * Builds a row component for a Section
+     * Builds a row component for a ListSection
      *
      * @param id - The id of the row. Maximum length: 200 characters.
      * @param title - The title of the row. Maximum length: 24 characters.
@@ -415,7 +459,7 @@ export class Row {
 }
 
 // TS knowledge intensifies
-function isSections(obj: unknown[]): obj is ProductSection[] {
+function isProductSections(obj: unknown[]): obj is ProductSection[] {
     return obj[0] instanceof ProductSection;
 }
 
@@ -457,7 +501,7 @@ export class ActionCatalog implements ClientTypedMessageComponent {
         catalog_id: string,
         ...products: [Product] | AtLeastOne<ProductSection>
     ) {
-        const is_sections = isSections(products);
+        const is_sections = isProductSections(products);
 
         if (is_sections) {
             if (products.length > 1) {
@@ -487,11 +531,7 @@ export class ActionCatalog implements ClientTypedMessageComponent {
  *
  * @group Interactive
  */
-export class ProductSection extends ClientLimitedComponent<Product, 30> {
-    /**
-     * The title of the section
-     */
-    readonly title?: string;
+export class ProductSection extends Section<Product, 30> {
     /**
      * The products of the section
      */
@@ -505,13 +545,8 @@ export class ProductSection extends ClientLimitedComponent<Product, 30> {
      * @throws If title is over 24 characters if provided
      * @throws If more than 30 products are provided
      */
-    constructor(title: string, ...products: AtLeastOne<Product>) {
-        super("Section", "products", products, 30);
-
-        if (title && title.length > 24)
-            throw new Error("Section title must be 24 characters or less");
-
-        if (title) this.title = title;
+    constructor(title: string | undefined, ...products: AtLeastOne<Product>) {
+        super("ProductSection", "products", products, 30, title);
         this.product_items = products;
     }
 }
