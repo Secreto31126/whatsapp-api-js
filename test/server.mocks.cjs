d1e83aecef0b909bb6a7c74010c1833167d1c054
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-09-03 03:23:57 -0300

    Minor performance change
    Closes #206

--- test/server.mocks.cjs
+++ test/server.mocks.cjs
@@ -2,7 +2,10 @@
 
 const { MockAgent } = require("undici");
 
-const agent = new MockAgent();
+const agent = new MockAgent({
+    keepAliveTimeout: 10,
+    keepAliveMaxTimeout: 10
+});
 agent.disableNetConnect();
 
 const clientFacebook = agent.get("https://graph.facebook.com");
