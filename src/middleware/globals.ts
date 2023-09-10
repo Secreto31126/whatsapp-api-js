97e209654bd7194cd3492718ade8ee1f0832d11f
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-09-10 18:58:54 -0300

    Improved the middlewares' docs

--- src/middleware/globals.ts
+++ src/middleware/globals.ts
@@ -6,11 +6,17 @@ import WhatsAppAPI from "../index.js";
  */
 export abstract class WhatsAppAPIMiddleware extends WhatsAppAPI {
     /**
+     * This method should be called when the server receives a POST request.
+     * Each child implements it differently depending on the framework.
+     *
      * @returns The status code to be sent to the client
      */
     abstract handle_post(...a: unknown[]): Promise<number>;
 
     /**
+     * This method should be called when the server receives a GET request.
+     * Each child implements it differently depending on the framework.
+     *
      * @returns The challenge string to be sent to the client
      * @throws The error code
      */
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,6 +1,36 @@
 // This file is a stud for the documentation.
 // You shouldn't import it as it might execute code for different frameworks.
 
+/**
+ * @module middleware
+ *
+ * @description
+ * Simplify the setup proccess of WhatsAppAPI for different frameworks.
+ *
+ * @example
+ * ```ts
+ * import WhatsAppAPI from "whatsapp-api-js/middleware/deno";
+ *
+ * const Whatsapp = new WhatsAppAPI({
+ *     token: "YOUR_TOKEN",
+ *     appSecret: "YOUR_APP_SECRET"
+ * });
+ *
+ * Deno.serve(async (req) => {
+ *     if (req.method === "POST") {
+ *         const status = await Whatsapp.handle_post(req);
+ *         return new Response(null, { status });
+ *     } else if (req.method === "GET") {
+ *         const challenge = Whatsapp.handle_get(req);
+ *         return new Response(challenge);
+ *     }
+ * });
+ * ```
+ *
+ * @see {@link WhatsAppAPIMiddleware.handle_post}
+ * @see {@link WhatsAppAPIMiddleware.handle_get}
+ */
+
 export { WhatsAppAPIMiddleware } from "./globals.js";
 export { default as ExpressMiddleware } from "./express.js";
 export { default as AdonisMiddleware } from "./adonis.js";
