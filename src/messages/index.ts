caf164533a74496f978b8e337d44ea577d633a30
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-09-10 22:36:55 -0300

    Improved messages' docs

--- src/messages/index.ts
+++ src/messages/index.ts
@@ -1,3 +1,19 @@
+/**
+ * @module messages
+ *
+ * @description
+ * A collection of classes to construct WhatsApp API messages.
+ *
+ * There are 7 types of messages:
+ * - {@link Text}
+ * - {@link Media}
+ * - {@link Template}
+ * - {@link Interactive}
+ * - {@link Location}
+ * - {@link Contacts}
+ * - {@link Reaction}
+ */
+
 export { default as Text } from "./text.js";
 export { default as Location } from "./location.js";
 export { default as Reaction } from "./reaction.js";
