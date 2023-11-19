f632e710d1a681a1493e50c83625a0dc90915a4c
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-11-19 04:45:18 -0300

    Update template.ts

--- src/messages/template.ts
+++ src/messages/template.ts
@@ -73,6 +73,7 @@ export class Template extends ClientMessage {
             | BodyComponent
             | ButtonComponent
             | CarouselComponent
+            | LTOComponent
         >
     >;
 
@@ -100,6 +101,7 @@ export class Template extends ClientMessage {
             | BodyComponent
             | ButtonComponent
             | CarouselComponent
+            | LTOComponent
         )[]
     ) {
         super();
@@ -479,7 +481,7 @@ export class CopyComponent extends ButtonComponent {
     /**
      * Creates a button component for a Template message with copy coupon button.
      *
-     * @param parameters - The variable for each url button.
+     * @param parameters - The coupon's code of the button to copy.
      * @throws If parameter is an empty string.
      */
     constructor(parameter: string) {
