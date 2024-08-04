2a59d66dc7882b2fd2398004d1261aaf804e4025
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-08-04 04:13:04 -0300

    ESLint fix

--- src/types.ts
+++ src/types.ts
@@ -303,6 +303,7 @@ export interface InteractiveAction extends ClientTypedMessageComponent {
         | "location_request_message";
 }
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface TemplateComponent extends ClientBuildableMessageComponent {}
 
 export type ClientMessageNames =
