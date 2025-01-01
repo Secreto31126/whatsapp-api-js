e6022cf76e467c039b5fad6542e4b0b809e06d8e
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-01-01 17:55:34 -0300

    Removed unused function

--- src/utils.ts
+++ src/utils.ts
@@ -2,10 +2,6 @@ export type AtLeastOne<T> = [T, ...T[]];
 
 export type MaybePromise<T> = T | Promise<T> | PromiseLike<T>;
 
-export function isInteger(n: unknown): n is number {
-    return Number.isInteger(n);
-}
-
 export function escapeUnicode(str: string) {
     // https://stackoverflow.com/a/40558081
     return str.replace(/[^\0-~]/g, (ch) => {
