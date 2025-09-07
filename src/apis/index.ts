eef456758b8af2cd5bf8678533de87371f296772
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-07 05:25:09 -0300

    Split the docs in separate files
    TS interfaces aren't as strong as Java's, but they are really cool still
    
    index.ts is 30% shorter than it used to be

---
+++ src/apis/index.ts
@@ -0,0 +1,6 @@
+export * as Message from "./message.js";
+export * as Call from "./call.js";
+export * as QR from "./qr.js";
+export * as Media from "./media.js";
+export * as Block from "./block.js";
+export * as Webhook from "./webhook.js";
