ab7fd03838a126af1734fce2572067748fe8e8c7
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-07-17 06:20:53 -0300

    Add payload length checks

--- src/middleware/globals.ts
+++ src/middleware/globals.ts
@@ -6,6 +6,21 @@ import { WhatsAppAPI } from "../index.js";
  */
 export abstract class WhatsAppAPIMiddleware extends WhatsAppAPI<void> {
     /**
+     * The max payload size received from the client.
+     * If the message is longer than the limit, the API
+     * will throw 413 to prevent a DoS attack.
+     *
+     * It's strongly advised not to increase this value
+     * over 3mb, as it's the upper limit from the API, but
+     * if you really have to, just edit this variable.
+     *
+     * Thanks @EQSTLab for the report!
+     *
+     * @see https://developers.facebook.com/documentation/business-messaging/whatsapp/webhooks/overview#payload-size
+     */
+    public static _MAX_PAYLOAD_SIZE = 3 * 1024 * 1024;
+
+    /**
      * This method should be called when the server receives a POST request.
      * Each child implements it differently depending on the framework.
      *
