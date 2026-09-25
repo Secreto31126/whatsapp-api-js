a868ff42c48ebd0817efb04451f3d8a6e836ef4f
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-09-25 15:54:12 -0300

    Cleaner test

--- test/index.test.js
+++ test/index.test.js
@@ -2012,15 +2012,7 @@ describe("WhatsAppAPI", () => {
                 await rejects(
                     Whatsapp.removeGroupParticipants(
                         group,
-                        { phone: "1" },
-                        { phone: "2" },
-                        { phone: "3" },
-                        { phone: "4" },
-                        { phone: "5" },
-                        { phone: "6" },
-                        { phone: "7" },
-                        { phone: "8" },
-                        { phone: "9" }
+                        new Array(9).fill({ phone: "1" })
                     )
                 );
             });
